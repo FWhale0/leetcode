cf4236e9519afa60d9ac05b60eb4814e8c3a1efe
FWhale0 <ifwhale@outlook.com>
2025-07-03 12:25:29 +0800

    Finished 1 with O(n)

--- 1.两数之和.py
+++ 1.两数之和.py
@@ -7,9 +7,19 @@
 # @lc code=start
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)-1):
-            for j in range(i+1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+        num_dict = {}
+        for loc, num in enumerate(nums):
+            if num in num_dict:
+                num_dict[num].append(loc)
+            else:
+                num_dict[num] = [loc]
+        for i in num_dict:
+            if target-i in num_dict and target-i != i:
+                return [num_dict[i][0], num_dict[target-i][0]]
+            if target-i in num_dict and target-i == i:
+                if len(num_dict[i]) == 1:
+                    continue
+                else:
+                    return[num_dict[i][0], num_dict[target-i][1]]
 # @lc code=end
 
